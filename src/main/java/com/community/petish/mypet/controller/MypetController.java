5784a0b47bcba0827e2ba55511e6edf4ae07c195
jaeju.jang <jjj0611@gmail.com>
2019-07-08 21:45:07 +0900

    [ADD] 마이펫 게시판을 담당하는 마이펫 컨트롤러를 생성하였습니다.

---
+++ src/main/java/com/community/petish/mypet/controller/MypetController.java
@@ -0,0 +1,12 @@
+package com.community.petish.mypet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypet/*")
+public class MypetController {
+
+	
+	
+}
