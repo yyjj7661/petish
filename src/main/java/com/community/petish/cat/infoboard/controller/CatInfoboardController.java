afc4753ff52848e22036eb15af368bf4f9986f3f
jaeju.jang <jjj0611@gmail.com>
2019-07-08 17:49:08 +0900

    [ADD] 고양이 정보 게시판 리스트를 위한 메서드를 컨트롤러에 추가하였습니다.

--- src/main/java/com/community/petish/cat/infoboard/controller/CatInfoboardController.java
+++ src/main/java/com/community/petish/cat/infoboard/controller/CatInfoboardController.java
@@ -6,5 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/cat/infoboard/*")
 public class CatInfoboardController {
+	
+	@RequestMapping("/list")
+	public String dogInfoboardList() {
+		return "/petish/cat/infoboard/list";
+	}
 
 }
