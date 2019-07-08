67f25ef2d32d9030822583456b949b00de515bd6
jaeju.jang <jjj0611@gmail.com>
2019-07-08 17:41:29 +0900

    [ADD] 고양이 정모게시판을 위한 컨트롤러를 추가하였습니다.

---
+++ src/main/java/com/community/petish/cat/gatherboard/controller/CatGatherboardController.java
@@ -0,0 +1,10 @@
+package com.community.petish.cat.gatherboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cat/gatherboard/*")
+public class CatGatherboardController {
+
+}
