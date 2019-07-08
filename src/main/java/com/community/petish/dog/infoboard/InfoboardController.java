6a8dcc80886a2d413e7708934468e497a3117b5d
jaeju.jang <jjj0611@gmail.com>
2019-07-08 16:50:00 +0900

    [ADD] dog 정보게시판을 위한 컨트롤러를 추가하였습니다.

---
+++ src/main/java/com/community/petish/dog/infoboard/InfoboardController.java
@@ -0,0 +1,12 @@
+package com.community.petish.dog.infoboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dog/infoboard/*")
+public class InfoboardController {
+	
+	
+
+}
