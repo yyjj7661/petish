d388bd98badfb0b87c07b540a7c0f925db7aa3bf
jaeju.jang <jjj0611@gmail.com>
2019-07-08 15:19:43 +0900

    [ADD] dog 자유게시판을 위한 컨트롤러를 생성하였습니다.

---
+++ src/main/java/com/community/petish/dog/freeboard/FreeboardController.java
@@ -0,0 +1,10 @@
+package com.community.petish.dog.freeboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dog/freeboard")
+public class FreeboardController {
+
+}
