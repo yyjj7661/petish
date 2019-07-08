2897a7c56f624d35f8957ec979746fd21578c58f
jaeju.jang <jjj0611@gmail.com>
2019-07-08 17:35:50 +0900

    [EDIT] 고양이 자유게시판의 controller의 이름이 강아지 자유게시판 controller의 이름과 중복되어 이름을 수정하였습니다.

--- src/main/java/com/community/petish/cat/freeboard/controller/CatFreeboardController.java
+++ src/main/java/com/community/petish/cat/freeboard/controller/CatFreeboardController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/cat/freeboard/*")
-public class FreeboardController {
+public class CatFreeboardController {
 
 	@RequestMapping("/list")
 	public String dogFreeboardList() {
