f265f882a79ff48f1471c600629ae028bbb86374
jaeju.jang <jjj0611@gmail.com>
2019-07-08 17:18:54 +0900

    [ADD] 실종견 게시판을 위한 컨트롤러를 추가하였습니다.

---
+++ src/main/java/com/community/petish/dog/missingboard/controller/MissingboardController.java
@@ -0,0 +1,12 @@
+package com.community.petish.dog.missingboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dog/missingboard/*")
+public class MissingboardController {
+
+	
+	
+}
