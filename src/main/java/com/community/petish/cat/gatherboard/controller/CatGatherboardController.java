5fe8c1e2e2b41290fbab68eda6c6f8bee6e7a70f
jaeju.jang <jjj0611@gmail.com>
2019-07-08 18:37:43 +0900

    [ADD] 고양이 정모 게시판 글 작성을 위한 메서드를 컨트롤러에 추가하였습니다.

--- src/main/java/com/community/petish/cat/gatherboard/controller/CatGatherboardController.java
+++ src/main/java/com/community/petish/cat/gatherboard/controller/CatGatherboardController.java
@@ -12,4 +12,9 @@ public class CatGatherboardController {
 		return "petish/cat/gatherboard/list";
 	}
 	
+	@RequestMapping("/writeForm")
+	public String dogGatherboardWriteForm() {
+		return "petish/cat/gatherboard/write_form";
+	}
+	
 }
