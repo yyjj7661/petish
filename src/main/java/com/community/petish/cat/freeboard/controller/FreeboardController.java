fc8ef7291493aabed88202dac638a8a83c9e6cdd
jaeju.jang <jjj0611@gmail.com>
2019-07-08 17:29:46 +0900

    [ADD] 고양이 자유게시판을 위한 컨트롤러를 추가하였습니다.

---
+++ src/main/java/com/community/petish/cat/freeboard/controller/FreeboardController.java
@@ -0,0 +1,10 @@
+package com.community.petish.cat.freeboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/dog/freeboard/*")
+public class FreeboardController {
+
+}
