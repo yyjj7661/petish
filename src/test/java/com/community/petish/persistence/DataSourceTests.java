2b593e871b1dd44dff303ae36c6ec1475bf36eda
jaeju.jang <jjj0611@gmail.com>
2019-07-15 03:16:33 +0900

    [EDIT] DataSourceTest를 Junit5를 사용하여 실행하도록 하기 위해 RunWith를 ExtendWith(SpringExtension.class)로 변경하였습니다.

--- src/test/java/com/community/petish/persistence/DataSourceTests.java
+++ src/test/java/com/community/petish/persistence/DataSourceTests.java
@@ -10,14 +10,15 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import lombok.extern.log4j.Log4j;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
 public class DataSourceTests {
