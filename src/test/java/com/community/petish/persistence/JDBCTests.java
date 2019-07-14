ef5986b99c2feadf3ecdef5ae6fd165c3ef5bd6a
jaeju.jang <jjj0611@gmail.com>
2019-07-15 00:52:44 +0900

    [ADD] DataBase 연동을 위한 JDBC 커넥션 테스트 클래스를 작성하고, 테스트를 완료하였습니다.

---
+++ src/test/java/com/community/petish/persistence/JDBCTests.java
@@ -0,0 +1,44 @@
+package com.community.petish.persistence;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests {
+
+	static {
+	
+		try { 
+		
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+	
+		} catch ( ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		
+		try (Connection con =
+					DriverManager.getConnection(
+							"jdbc:oracle:thin:@localhost:31521:XE",
+							"petish",
+							"petish")) {
+			log.info(con);
+		} catch (SQLException e) {
+			
+			fail(e.getMessage());			
+		
+		}
+		
+	}
+	
+}
