74840db421b81290c00966c54d4352ed787a6bc4
jaeju.jang <jjj0611@gmail.com>
2019-07-15 21:37:51 +0900

    [ADD] UserMapper의 insert 기능을 확인하기 위한 테스트를 추가하였습니다.

--- src/test/java/com/community/petish/user/mapper/UserMapperTest.java
+++ src/test/java/com/community/petish/user/mapper/UserMapperTest.java
@@ -1,10 +1,16 @@
 package com.community.petish.user.mapper;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.community.petish.user.dto.request.JoinUserParams;
+import com.community.petish.user.dto.response.UserDetailResponse;
 
 import lombok.extern.log4j.Log4j;
 
@@ -19,6 +25,17 @@ public class UserMapperTest {
 	@Test
 	void userMapperTest() {
 		log.info(userMapper);
+	}
+	
+	@Test
+	void saveUser() {
+		String imgaddr = "https://image.fmkorea.com/files/attach/new/20181128/486616/796418645/1413259662/fa0f4a56ff0bc3e2d25ab1f3c6e42fc7.jpeg";
+		JoinUserParams joinUserParams = new JoinUserParams("jjj0611@hanmail.net", "1234", "dipord", "서울 동대문구 휘경동 183-108번지", "남자", imgaddr, "강아지");
+		userMapper.save(joinUserParams);
+		
+		UserDetailResponse userDetailResponse = userMapper.findByEmail("jjj0611@hanmail.net");
+		
+		assertThat(joinUserParams.getNickname()).isEqualTo(userDetailResponse.getNickname());
 	}
 	
 }
