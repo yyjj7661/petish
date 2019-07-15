ec0a088dec2dc86c6cbaac8631507061fb2e76af
jaeju.jang <jjj0611@gmail.com>
2019-07-15 21:24:46 +0900

    [ADD] user의 response를 위한 dto 객체 UserDetailResponse를 생성하였습니다.

---
+++ src/main/java/com/community/petish/user/dto/response/UserDetailResponse.java
@@ -0,0 +1,14 @@
+package com.community.petish.user.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class UserDetailResponse {
+
+	private String email;
+	private String nickname;
+	private String gender;
+	private String profileImage;
+	private String concern;
+	
+}
