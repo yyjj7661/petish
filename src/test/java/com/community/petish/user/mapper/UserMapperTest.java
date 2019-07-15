0625496c416528502ac1bfd30fd7ae2fb9fda3d7
jaeju.jang <jjj0611@gmail.com>
2019-07-15 20:37:55 +0900

    [ADD] UserMapper를 테스트할 테스트 클래스를 만들고, UserMapper를 Autowired하는 코드를 작성하였습니다.

---
+++ src/test/java/com/community/petish/user/mapper/UserMapperTest.java
@@ -0,0 +1,18 @@
+package com.community.petish.user.mapper;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import lombok.extern.log4j.Log4j;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class UserMapperTest {
+
+	@Autowired
+	UserMapper userMapper;
+	
+}
