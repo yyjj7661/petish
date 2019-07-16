bead9e60339eba0e7d38a9dc51f0942d5f34989d
jaeju.jang <jjj0611@gmail.com>
2019-07-17 00:08:49 +0900

    [ADD] UserMapper의 user를 저장하는 save와 username으로 검색하는 기능을 테스트하는 메서드를 생성하였습니다.

--- src/test/java/com/community/petish/user/mapper/UserMapperTest.java
+++ src/test/java/com/community/petish/user/mapper/UserMapperTest.java
@@ -3,6 +3,7 @@ package com.community.petish.user.mapper;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,11 +31,11 @@ public class UserMapperTest {
 	@Test
 	void saveUser() {
 		String imgaddr = "https://image.fmkorea.com/files/attach/new/20181128/486616/796418645/1413259662/fa0f4a56ff0bc3e2d25ab1f3c6e42fc7.jpeg";
-		JoinUserParams joinUserParams = new JoinUserParams("jjj0611@hanmail.net", "1234", "dipord", "서울 동대문구 휘경동 183-108번지", "남자", imgaddr, "강아지");
+		JoinUserParams joinUserParams = new JoinUserParams("jjj0611@hanmail.net", "1234", "dipord", "서울 동대문구 휘경동 183-108번지", "남자", imgaddr, "dog");
 		userMapper.save(joinUserParams);
 		
 		UserDetailResponse userDetailResponse = userMapper.findByUsername("jjj0611@hanmail.net");
-		
+		log.info(userDetailResponse);
 		assertThat(joinUserParams.getNickname()).isEqualTo(userDetailResponse.getNickname());
 	}
 	
