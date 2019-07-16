4d44aa200e26016d098c19c29c79d6dd090751b6
jaeju.jang <jjj0611@gmail.com>
2019-07-17 00:10:40 +0900

    [ADD] DB에서 데이터를 가져오는 Repository를 위한 인터페이스를 추가하고, 유저를 추가하고, username으로 유저를 검색하는 메서드를 추가하였습니다.

--- src/main/java/com/community/petish/user/mapper/UserMapper.java
+++ src/main/java/com/community/petish/user/mapper/UserMapper.java
@@ -1,5 +1,12 @@
 package com.community.petish.user.mapper;
 
+import com.community.petish.user.dto.request.JoinUserParams;
+import com.community.petish.user.dto.response.UserDetailResponse;
+
 public interface UserMapper {
 
+	Long save(JoinUserParams joinUserParams);
+	
+	UserDetailResponse findByUsername(String username);
+	
 }
