9f8419172585e99561ef0ac7bb45c4e0a544fb9f
jaeju.jang <jjj0611@gmail.com>
2019-07-17 14:07:41 +0900

    [ADD] UserService를 테스트하기 위한 테스트를 생성하였습니다.

---
+++ src/test/java/com/community/petish/user/mapper/UserServiceTest.java
@@ -0,0 +1,22 @@
+package com.community.petish.user.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.community.petish.user.service.UserService;
+
+import lombok.extern.log4j.Log4j2;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j2
+public class UserServiceTest {
+
+	
+	@Autowired
+	UserService userService;
+	
+}
