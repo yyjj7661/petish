71108f7639f91ae1b53adad25150bf37ac45fe14
jaeju.jang <jjj0611@gmail.com>
2019-07-17 17:35:30 +0900

    [ADD] SaveUserParams의 비밀번호를 encode하기 위해 @Setter를 추가하였습니다.

--- src/main/java/com/community/petish/user/dto/request/SaveUserParams.java
+++ src/main/java/com/community/petish/user/dto/request/SaveUserParams.java
@@ -3,9 +3,11 @@ package com.community.petish.user.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
