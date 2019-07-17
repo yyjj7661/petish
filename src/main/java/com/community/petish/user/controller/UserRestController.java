cb922bcae9ed123126037862444b89eb7c4a074d
jaeju.jang <jjj0611@gmail.com>
2019-07-17 12:24:52 +0900

    [ADD] UserRestController에서 사용할 UserService 객체를 Autowired하였습니다.

--- src/main/java/com/community/petish/user/controller/UserRestController.java
+++ src/main/java/com/community/petish/user/controller/UserRestController.java
@@ -1,10 +1,21 @@
 package com.community.petish.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.community.petish.user.dto.request.SaveUserParams;
+import com.community.petish.user.service.UserService;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
+	
+	@Autowired
+	UserService userService;
+	
+	
 
 }
