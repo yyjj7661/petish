e1c914af262cb01fab70265c5a893de2632ac584
jaeju.jang <jjj0611@gmail.com>
2019-07-17 14:22:59 +0900

    [ADD] UserService Test에서 유저를 추가하는 테스트 케이스를 추가하였습니다.

--- src/test/java/com/community/petish/user/service/UserServiceTest.java
+++ src/test/java/com/community/petish/user/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.community.petish.user.dto.request.SaveUserParams;
 import com.community.petish.user.service.UserService;
 
 import lombok.extern.log4j.Log4j2;
@@ -21,6 +22,12 @@ public class UserServiceTest {
 	
 	@Test
 	void saveUserTest() {
+		String imgaddr = "https://image.fmkorea.com/files/attach/new/20181128/486616/796418645/1413259662/fa0f4a56ff0bc3e2d25ab1f3c6e42fc7.jpeg";
+		SaveUserParams saveUserParams = new SaveUserParams("jjj0611@hanmail.net", "1234", "dipord", "서울 동대문구 휘경동 183-108번지", "남자", imgaddr, "dog");
+		
+		Long userId = userService.saveUser(saveUserParams);
+		
+		log.info("새롭게 생성된 유저 아이디 : " + userId);
 		
 	}
 }
