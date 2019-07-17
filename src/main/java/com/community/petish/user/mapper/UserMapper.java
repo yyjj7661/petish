08bcea7e533555f9d40abe9b2f113e31f21fe05c
jaeju.jang <jjj0611@gmail.com>
2019-07-17 15:35:59 +0900

    [ADD] UserMapper가 id 값으로 user를 찾고, User 도메인 객체를 반환하는 메서드를 선언하고, 쿼리문을 작성하였습니다.

--- src/main/java/com/community/petish/user/mapper/UserMapper.java
+++ src/main/java/com/community/petish/user/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.community.petish.user.mapper;
 
+import com.community.petish.user.domain.User;
 import com.community.petish.user.dto.request.SaveUserParams;
 import com.community.petish.user.dto.response.UserDetailResponse;
 
@@ -8,6 +9,8 @@ public interface UserMapper {
 	Long save(SaveUserParams saveUserParams);
 	
 	UserDetailResponse findByUsername(String username);
+	
+	User findById(Long id);
 	
 	void deleteAll();
 	
