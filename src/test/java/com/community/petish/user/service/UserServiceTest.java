d5d0ba8a270e7fadead47cbdae29a61f8e9854bb
jaeju.jang <jjj0611@gmail.com>
2019-07-17 15:38:17 +0900

    [ADD] UserService의 테스트 종료시 user 테이블을 모두 삭제하기 위해 UserMapper를 추가하고, user 데이터를 모두 삭제하는 메서드를 테스트 실행시마다 실행하도록 하였습니다.

--- src/test/java/com/community/petish/user/service/UserServiceTest.java
+++ src/test/java/com/community/petish/user/service/UserServiceTest.java
@@ -1,14 +1,17 @@
 package com.community.petish.user.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.community.petish.user.domain.User;
 import com.community.petish.user.dto.request.SaveUserParams;
-import com.community.petish.user.dto.response.UserDetailResponse;
-import com.community.petish.user.service.UserService;
+import com.community.petish.user.mapper.UserMapper;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -21,6 +24,14 @@ public class UserServiceTest {
 	@Autowired
 	UserService userService;
 	
+	@Autowired
+	UserMapper userMapper;
+	
+	@AfterEach
+	void deleteUsers() {
+		userMapper.deleteAll();
+	}
+	
 	@Test
 	void saveUserTest() {
 		String imgaddr = "https://image.fmkorea.com/files/attach/new/20181128/486616/796418645/1413259662/fa0f4a56ff0bc3e2d25ab1f3c6e42fc7.jpeg";
@@ -31,6 +42,10 @@ public class UserServiceTest {
 		log.info("새롭게 생성된 유저 아이디 : " + userId);
 		
 		User resultUser = userService.findById(userId);
+		
+		assertThat(resultUser.getId()).isEqualTo(userId);
+		assertThat(resultUser.getUsername()).isEqualTo(saveUserParams.getUsername());
+		
 		
 	}
 }
