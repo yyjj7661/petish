21358b08329970f2ef0cbb135da15517149eefd7
jaeju.jang <jjj0611@gmail.com>
2019-07-17 20:36:46 +0900

    [ADD] 모든 유저를 불러오는 메서드를 UserService 인터페이스에 추가하였습니다.

--- src/main/java/com/community/petish/user/service/UserService.java
+++ src/main/java/com/community/petish/user/service/UserService.java
@@ -2,11 +2,14 @@ package com.community.petish.user.service;
 
 import com.community.petish.user.domain.User;
 import com.community.petish.user.dto.request.SaveUserParams;
+import com.community.petish.user.dto.response.UserListResponse;
 
 public interface UserService {
 
 	Long saveUser(SaveUserParams saveUserParams);
 
 	User findById(Long id);
+	
+	UserListResponse getUsers();
 	
 }
