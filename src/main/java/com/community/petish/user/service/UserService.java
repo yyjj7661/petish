ef6a0641116b75392a5a242429a2792b607941d8
jaeju.jang <jjj0611@gmail.com>
2019-07-17 15:22:34 +0900

    [ADD] UserService에 유저를 저장하는 saveUser 메서드를 추가하고, 해당 메서드를 구현하였습니다.

--- src/main/java/com/community/petish/user/service/UserService.java
+++ src/main/java/com/community/petish/user/service/UserService.java
@@ -1,7 +1,9 @@
 package com.community.petish.user.service;
 
+import com.community.petish.user.dto.request.SaveUserParams;
+
 public interface UserService {
 
-	
+	Long saveUser(SaveUserParams saveUserParams);
 	
 }
--- src/main/java/com/community/petish/user/service/UserServiceImpl.java
+++ src/main/java/com/community/petish/user/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.community.petish.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.community.petish.user.dto.request.SaveUserParams;
 import com.community.petish.user.mapper.UserMapper;
 
 @Service
@@ -10,5 +11,12 @@ public class UserServiceImpl implements UserService{
 
 	@Autowired
 	UserMapper userMapper;
+
+	@Override
+	public Long saveUser(SaveUserParams saveUserParams) {
+		
+		
+		return userMapper.save(saveUserParams);
+	}
 	
 }
