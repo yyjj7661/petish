0e7347e1647f678a55fd2138f0ff86accdde8ba5
jaeju.jang <jjj0611@gmail.com>
2019-07-17 12:27:49 +0900

    [ADD] UserService에서 사용할 UserMapper 클래스를 Autowired로 자동주입 받았습니다.

--- src/main/java/com/community/petish/user/service/UserServiceImpl.java
+++ src/main/java/com/community/petish/user/service/UserServiceImpl.java
@@ -1,8 +1,14 @@
 package com.community.petish.user.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.community.petish.user.mapper.UserMapper;
 
 @Service
 public class UserServiceImpl implements UserService{
 
+	@Autowired
+	UserMapper userMapper;
+	
 }
