62b76b778961be04b3d14b2571e20490fa3d3a46
jaeju.jang <jjj0611@gmail.com>
2019-07-17 15:21:22 +0900

    [EDIT] User 도메인 객체의 인스턴스 변수를 추가하였습니다.

--- src/main/java/com/community/petish/user/domain/User.java
+++ src/main/java/com/community/petish/user/domain/User.java
@@ -1,7 +1,23 @@
 package com.community.petish.user.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import oracle.sql.DATE;
+
+@Getter
+@AllArgsConstructor
 public class User {
 
-	
+	private Long id;
+	private String username;
+	private String password;
+	private String nickname;
+	private String address;
+	private String gender;
+	private String picture;
+	private DATE join_date;
+	private DATE updated_date;
+	private int deleted;
+	private int concern_id;
 	
 }
