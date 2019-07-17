f4012b3d0b34bf1dd4cd6f38736ad1a29fb03a64
july5087 <july5087@gmail.com>
2019-07-17 14:30:41 +0900

    [ADD] 실종견 게시판의 VO클래스를 생성했습니다.

--- src/main/java/com/community/petish/dog/missingboard/domain/DogLostPostVO.java
+++ src/main/java/com/community/petish/dog/missingboard/domain/DogLostPostVO.java
@@ -2,6 +2,8 @@ package com.community.petish.dog.missingboard.domain;
 
 import java.util.Date;
 
+import lombok.Data;
+
 /* 실종견 게시판 게시글 */
 /*
 CREATE TABLE dog_lost_post_tb (
@@ -26,27 +28,31 @@ CREATE TABLE dog_lost_post_tb (
 );
 */
 
+@Data
 public class DogLostPostVO {
 	
-	private String id; //게시글 번호
+	private String ID; //게시글 번호
 
-	private String dog_name; //강아지 이름
-	private int dog_age; //강아지 나이
-	private String dog_gender; //강아지 성별 
-	private String dog_descrption; //강아지 특징
-	private String dog_image; //강아지 사진
-	private Date dog_lost_date; //실종 일시
-	private String phone_number; //연락처
-	private String dog_lost_address; //실종 장소
-	private int reward; //사례금
-	private int view_count; //조회수
-	private Date create_date; //게시글 등록 시간
-	private Date updated_date; //게시글 수정 시간
-	private int found; //발견
-	private int deleted; //삭제 유무
+	private String DOG_NAME; //강아지 이름
+	private int DOG_AGE; //강아지 나이
+	private String DOG_GENDER; //강아지 성별 
+	private String DOG_DESCRIPTION; //강아지 특징
+	private String DOG_IMAGE; //강아지 사진
+	
+	private Date DOG_LOST_DATE; //실종 일시
+	private String PHONE_NUMBER; //연락처
+	private String DOG_LOST_ADDRESS; //실종 장소
+	private int REWARD; //사례금
+	
+	private int VIEW_COUNT; //조회수
+	private Date CREATE_DATE; //게시글 등록 시간
+	private Date UPDATED_DATE; //게시글 수정 시간
+	
+	private int FOUND; //발견
+	private int DELETED; //삭제 유무
 
-	private int region_id; //지역 식별자
-	private int user_id; //유저 식별자
-	private int specied_id; //강아지 종류 식별자
+	private int REGION_ID; //지역 식별자
+	private int USER_ID; //유저 식별자
+	private int SPECIES_ID; //강아지 종류 식별자
 	
 }
