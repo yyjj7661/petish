31931d2c78e1638987b4d131bbb9744c09901a68
yelim <yelim615@naver.com>
2019-07-17 14:39:05 +0900

    [UPDATE] Mypage controller, pom.xml(log4j오류) 수정반영

--- src/main/java/com/community/petish/mypage/controller/MypageController.java
+++ src/main/java/com/community/petish/mypage/controller/MypageController.java
@@ -1,12 +1,27 @@
 package com.community.petish.mypage.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.community.petish.mypage.dto.QuestionRequestDTO;
+import com.community.petish.mypage.dto.QuestionResponseDTO;
+import com.community.petish.mypage.service.QuestionService;
+
+import lombok.extern.log4j.Log4j;
+
 @Controller
+@Log4j
 @RequestMapping("/mypage/*")
 public class MypageController {
 
+
+	@Autowired
+	private QuestionService questionServiceImpl;
+	
 	@RequestMapping("/")
 	public String mypage() {
 		return "petish/mypage/default";
@@ -23,23 +38,34 @@ public class MypageController {
 	}
 	
 	@RequestMapping("/question/list")
-	public String questionList() {
+	public String questionList(Model model) {
+		ArrayList<QuestionRequestDTO> list = questionServiceImpl.getQuestionList(1);
+		model.addAttribute("list",list);
 		return "petish/mypage/question_list";
 	}
 	
-	@RequestMapping("/question/detail")
-	public String questionDetail() {
-		return "petish/mypage/question_detail";
-	}
-	
-	@RequestMapping("/question/writeForm")
-	public String questionWriteForm() {
-		return "petish/mypage/question_write_form";
-	}
 	
 	@RequestMapping("/member/detail")
 	public String memberDetail() {
 		return "petish/mypage/member_detail";
 	}
+
+	@RequestMapping("/question/insert")
+	public String insertQuestion(QuestionResponseDTO dto) {
+		log.info(dto);
+		QuestionResponseDTO dto2 = new QuestionResponseDTO();
+		dto2.setUser_id(dto.getUser_id());
+		dto2.setTitle(dto.getTitle());
+		dto2.setContent(dto.getContent());
+		dto2.setCategory_id(2);
+		questionServiceImpl.insertQuestion(dto2);
+		return "redirect:./list";
+	}
 	
+	@RequestMapping("/question/delete")
+	public String deleteQuestion(int id) {
+		log.info(id);
+		questionServiceImpl.deleteQuestion(id);
+		return "redirect:./list";
+	}
 }
