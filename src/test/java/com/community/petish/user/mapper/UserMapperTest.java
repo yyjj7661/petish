50f6f5f5c9accf211e8929c44b95f41546452b13
jaeju.jang <jjj0611@gmail.com>
2019-07-18 16:46:57 +0900

    [EDIT] userMapper의 save 메서드가 새롭게 생성된 유저의 id를 반환하기 때문에 해당 아이디로 유저를 불러오는 방식으로 테스트를 변경하였습니다.

--- src/test/java/com/community/petish/user/mapper/UserMapperTest.java
+++ src/test/java/com/community/petish/user/mapper/UserMapperTest.java
@@ -1,7 +1,6 @@
 package com.community.petish.user.mapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,8 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.community.petish.user.domain.User;
 import com.community.petish.user.dto.request.SaveUserParams;
-import com.community.petish.user.dto.response.UserDetailResponse;
 
 import lombok.extern.log4j.Log4j;
 
@@ -37,11 +36,11 @@ public class UserMapperTest {
 	void saveUser() {
 		String imgaddr = "https://image.fmkorea.com/files/attach/new/20181128/486616/796418645/1413259662/fa0f4a56ff0bc3e2d25ab1f3c6e42fc7.jpeg";
 		SaveUserParams saveUserParams = new SaveUserParams("jjj0611@hanmail.net", "1234", "dipord", "서울 동대문구 휘경동 183-108번지", "남자", imgaddr, "dog");
-		userMapper.save(saveUserParams);
+		Long id = userMapper.save(saveUserParams);
 		
-		UserDetailResponse userDetailResponse = userMapper.findByUsername("jjj0611@hanmail.net");
-		log.info("userDetailResponse : " + userDetailResponse);
-		assertThat(saveUserParams.getNickname()).isEqualTo(userDetailResponse.getNickname());
+		User user = userMapper.findById(id);
+		log.info("savedUser : " + user);
+		assertThat(saveUserParams.getNickname()).isEqualTo(user.getNickname());
 	}
 	
 }
