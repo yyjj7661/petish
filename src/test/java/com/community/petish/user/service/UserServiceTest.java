0548f79fa78dc9aaf5d0326ce12174624b199c0b
jaeju.jang <jjj0611@gmail.com>
2019-07-18 14:05:11 +0900

    [ADD] 모든 유저를 찾는 테스트를 UserServiceTest에 추가하였습니다.

--- src/test/java/com/community/petish/user/service/UserServiceTest.java
+++ src/test/java/com/community/petish/user/service/UserServiceTest.java
@@ -11,14 +11,15 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.community.petish.user.domain.User;
 import com.community.petish.user.dto.request.SaveUserParams;
+import com.community.petish.user.dto.response.UserListResponse;
 import com.community.petish.user.mapper.UserMapper;
 
 import lombok.extern.log4j.Log4j;
-import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml", "file:src/main/webapp/WEB-INF/spring/spring-security.xml"})
-@Log4j
+@Slf4j
 public class UserServiceTest {
 
 	
@@ -47,5 +48,23 @@ public class UserServiceTest {
 		assertThat(resultUser.getId()).isEqualTo(userId);
 		assertThat(resultUser.getUsername()).isEqualTo(saveUserParams.getUsername());
 		assertThat(resultUser.getNickname()).isEqualTo(saveUserParams.getNickname());
+	}
+	
+	@Test
+	void findAllTest() {
+		String imgaddr = "y";
+		SaveUserParams saveUserParams = new SaveUserParams("jjj0611@hanmail.net", "1234", "dipord", "서울 동대문구 휘경동 183-108번지", "남자", imgaddr, "dog");
+		
+		userService.saveUser(saveUserParams);
+		
+		String imgaddr2 = "https://image.ytn.co.kr/general/jpg/2017/1018/201710181100063682_d.jpg";
+		SaveUserParams saveUserParams2 = new SaveUserParams("djw627@gmail.com", "5678", "djw", "서울 동대문구 휘경동 209번지", "여", imgaddr2, "cat");
+		
+		userService.saveUser(saveUserParams2);
+		
+		UserListResponse userListResponse = userService.getUsers();
+
+		log.info("모든 유저 : " + userListResponse);
+
 	}
 }
