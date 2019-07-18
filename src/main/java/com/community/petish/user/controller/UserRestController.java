ef7f3034ba6df64ff112da6507b705cac9be8466
jaeju.jang <jjj0611@gmail.com>
2019-07-18 15:20:30 +0900

    [ADD] 모든 유저의 정보를 불러오는 Get 방식의 요청을 작성하였습니다.

--- src/main/java/com/community/petish/user/controller/UserRestController.java
+++ src/main/java/com/community/petish/user/controller/UserRestController.java
@@ -1,6 +1,7 @@
 package com.community.petish.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,9 @@ import com.community.petish.user.dto.request.SaveUserParams;
 import com.community.petish.user.dto.response.UserListResponse;
 import com.community.petish.user.service.UserService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserRestController {
@@ -18,12 +22,14 @@ public class UserRestController {
 	@Autowired
 	UserService userService;
 	
-	@GetMapping
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_UTF8_VALUE })
 	public UserListResponse getUsers()   {
-		return userService.getUsers();
+		UserListResponse userListResponse = userService.getUsers();
+		log.info(userListResponse.toString());
+		return userListResponse;
 	}
 	
-	@PostMapping
+	@PostMapping(consumes = {"application/json"})
 	public void save(@RequestBody SaveUserParams saveUserParams) {
 		userService.saveUser(saveUserParams);
 	}
