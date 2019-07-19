05f3073ba125c063e3ee28b8a35ccb472197aea9
july5087 <july5087@gmail.com>
2019-07-19 17:39:04 +0900

    [ADD] 실종견 게시판의 매퍼 클래스를 수정했습니다.

--- src/main/java/com/community/petish/dog/missingboard/mapper/DogLostPostMapper.java
+++ src/main/java/com/community/petish/dog/missingboard/mapper/DogLostPostMapper.java
@@ -2,8 +2,8 @@ package com.community.petish.dog.missingboard.mapper;
 
 import java.util.List;
 
-import com.community.petish.dog.missingboard.domain.DogLostPostVO;
 import com.community.petish.dog.missingboard.dto.DogLostPostRequestWriteDTO;
+import com.community.petish.dog.missingboard.dto.DogLostPostResponseDetailDTO;
 import com.community.petish.dog.missingboard.dto.DogLostPostResponseListDTO;
 
 
@@ -16,15 +16,16 @@ public interface DogLostPostMapper {
 	List<DogLostPostResponseListDTO> getDogLostList();
 	
 	//게시글 조회
-	DogLostPostVO getPostDetail(int num);
+	DogLostPostResponseDetailDTO getPostDetail(Long id);
 	
 	//게시글  작성
-	int insertDogLostPost(DogLostPostRequestWriteDTO dto);
+	int insertPost(DogLostPostRequestWriteDTO dto);
+	//int insertSelectKey(DogLostPostRequestWriteDTO dto);	
 	
 	//게시글 수정
-	int updateDogLostPost(DogLostPostRequestWriteDTO dto);
+	int updatePost(DogLostPostRequestWriteDTO dto);
 	
 	//게시글 삭제
-	int deleteDogLostPost(int num);
+	int deletePost(Long id);	
 	
 }
