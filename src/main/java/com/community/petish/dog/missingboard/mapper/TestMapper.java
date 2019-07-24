981a5db2a59a69480aaa24b87fa6dfc4aac68e54
july5087 <july5087@gmail.com>
2019-07-24 20:58:37 +0900

    [ADD] 실종견 게시판의 매퍼클래스를 수정했습니다.

--- src/main/java/com/community/petish/dog/missingboard/mapper/TestMapper.java
+++ src/main/java/com/community/petish/dog/missingboard/mapper/TestMapper.java
@@ -2,24 +2,24 @@ package com.community.petish.dog.missingboard.mapper;
 
 import java.util.List;
 
-import com.community.petish.dog.missingboard.domain.testVO;
+import com.community.petish.dog.missingboard.domain.TestVO;
 
-public interface testMapper {
+public interface TestMapper {
 	
 	//게시글 수
 	int getCount();
 	
 	//게시글 리스트
-	List<testVO> getPostList();
+	List<TestVO> getPostList();
 	
 	//게시글 조회
-	testVO getPostDetail(int num);
+	TestVO getPostDetail(int num);
 	
 	//게시글  작성
-	int insertTest(testVO vo);
+	int insertTest(TestVO vo);
 	
 	//게시글 수정
-	int updateTest(testVO vo);
+	int updateTest(TestVO vo);
 	
 	//게시글 삭제
 	int deleteTest(int num);
