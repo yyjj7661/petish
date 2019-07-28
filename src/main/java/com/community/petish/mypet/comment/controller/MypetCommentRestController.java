dde1e22acb26c866be406c592da63a0ec23a369b
jaeju.jang <jjj0611@gmail.com>
2019-07-29 01:36:46 +0900

    [ADD] mypet comment를 위한 RestController를 생성하고, request경로를 mapping하였습니다.

---
+++ src/main/java/com/community/petish/mypet/comment/controller/MypetCommentRestController.java
@@ -0,0 +1,15 @@
+package com.community.petish.mypet.comment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/comments")
+public class MypetCommentRestController {
+
+	
+	
+}
