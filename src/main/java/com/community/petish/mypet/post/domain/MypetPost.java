0c8aa99164aac5bea1bd84866e4b1d46e073dd2e
jaeju.jang <jjj0611@gmail.com>
2019-07-28 19:24:57 +0900

    [ADD] MypetPost에 대한 domain객체를 생성하였습니다.

---
+++ src/main/java/com/community/petish/mypet/post/domain/MypetPost.java
@@ -0,0 +1,16 @@
+package com.community.petish.mypet.post.domain;
+
+import java.sql.Date;
+
+public class MypetPost {
+
+	private Long id;
+	private String title;
+	private String content;
+	private String image;
+	private Date createdDate;
+	private Date updatedDate;
+	
+	private Long userId;
+	
+}
