807fa50ac4a58285644d01335649b49ced72368d
jaeju.jang <jjj0611@gmail.com>
2019-07-28 19:22:19 +0900

    [ADD] mypet post api 요청을 위한 MypetPostRestController를 만들고, 포스트를 저장하기 위한 savePost 메서드를 Post방식의 메서드로 선언하였습니다.

---
+++ src/main/java/com/community/petish/mypet/post/controller/MypetPostRestController.java
@@ -0,0 +1,34 @@
+package com.community.petish.mypet.post.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.community.petish.mypet.post.dto.response.MypetPostDetailResponse;
+import com.community.petish.mypet.post.dto.response.MypetPostSummaryList;
+import com.community.petish.mypet.post.service.MypetPostService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/mypet/posts")
+public class MypetPostRestController {
+	
+	@Autowired
+	MypetPostService mypetPostService;
+	
+	@PostMapping
+	public void savePost(MultipartHttpServletRequest request) throws Exception {
+		log.info("mypet Post upload 요청");
+		
+		Long postId = mypetPostService.savePost(request);
+		
+	}
+	
+}
