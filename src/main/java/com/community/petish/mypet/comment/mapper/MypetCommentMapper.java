ddbdcfb756de36cbdf234db7b8759bb1932ae3c4
jaeju.jang <jjj0611@gmail.com>
2019-07-29 16:06:34 +0900

    [ADD] mypet post id에 대하여 댓글 목록을 불러오는 메서드를 mapper에 추가하였습니다.

--- src/main/java/com/community/petish/mypet/comment/mapper/MypetCommentMapper.java
+++ src/main/java/com/community/petish/mypet/comment/mapper/MypetCommentMapper.java
@@ -1,9 +1,18 @@
 package com.community.petish.mypet.comment.mapper;
 
+import java.util.List;
+
+import com.community.petish.mypet.comment.domain.MypetComment;
 import com.community.petish.mypet.comment.dto.request.SaveMypetCommentParams;
 
 public interface MypetCommentMapper {
 
 	Long save(SaveMypetCommentParams saveMypetCommentParams);
+	
+	List<MypetComment> findAllByPostId(Long postId);
+	
+	Long countLikeByCommentId(Long commentId);
+	
+	Long countDislikeByCommentId(Long commentId);
 	
 }
