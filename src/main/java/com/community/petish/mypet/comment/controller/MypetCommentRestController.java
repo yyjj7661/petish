04b265dffeab61dd41c9f53d0f24309c2a125bf5
jaeju.jang <jjj0611@gmail.com>
2019-07-29 16:04:51 +0900

    [ADD] mypet post id에 대하여 댓글 목록을 불러오는 메서드를 RestController에 추가하였습니다.

--- src/main/java/com/community/petish/mypet/comment/controller/MypetCommentRestController.java
+++ src/main/java/com/community/petish/mypet/comment/controller/MypetCommentRestController.java
@@ -3,10 +3,14 @@ package com.community.petish.mypet.comment.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.community.petish.mypet.comment.dto.response.MypetCommentListResponse;
 import com.community.petish.mypet.comment.service.MypetCommentService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -26,4 +30,12 @@ public class MypetCommentRestController {
 		
 	}
 	
+	@GetMapping(value = "/post/{postId}", produces = { MediaType.APPLICATION_JSON_UTF8_VALUE } )
+	public MypetCommentListResponse getCommentsByPostId(@PathVariable("postId") Long postId) {
+		
+		MypetCommentListResponse mypetCommentListResponse = mypetCommentService.getCommentsByPostId(postId);
+		
+		return mypetCommentListResponse;
+		
+	}
 }
