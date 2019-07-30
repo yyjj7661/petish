d708d938981e7d4e4edd7268f11be46b1ed15b00
july5087 <july5087@gmail.com>
2019-07-30 09:38:00 +0900

    [ADD] 파일첨뿌 Mapper 인터페이스와 xml 파일에 첨부 파일을 삭제하는 메서드를 추가했습니다.

--- src/main/java/com/community/petish/dog/missingboard/mapper/AttachFileMapper.java
+++ src/main/java/com/community/petish/dog/missingboard/mapper/AttachFileMapper.java
@@ -5,9 +5,13 @@ import java.util.List;
 import com.community.petish.dog.missingboard.domain.AttachFileVO;
 
 public interface AttachFileMapper {
+	
 	public void insert(AttachFileVO vo);
 	
 	public void delete(String uuid);
 	
 	public List<AttachFileVO> findByPostId(Long postId);
+	
+	public void deleteAll(Long postId);
+	
 }
