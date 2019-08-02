e36452025b8f7a6134c52ab8d94e4d3050fb7998
jaeju.jang <jjj0611@gmail.com>
2019-08-02 09:25:06 +0900

    [ADD] UserRestControllerAdvice에 ExceptionHandler를 추가하였습니다.

--- src/main/java/com/community/petish/user/advice/UserRestControllerAdvice.java
+++ src/main/java/com/community/petish/user/advice/UserRestControllerAdvice.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.community.petish.user.exception.AuthenticationException;
+import com.community.petish.user.exception.PasswordNotMatchException;
+import com.community.petish.user.exception.UserNotFoundException;
 
 @RestControllerAdvice
 public class UserRestControllerAdvice {
@@ -13,6 +15,18 @@ public class UserRestControllerAdvice {
 	@ExceptionHandler(AuthenticationException.class)
 	@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
 	public void authenticationExceptionHandler(AuthenticationException ex) {
+		
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public void userNotFoundExceptionHandler(UserNotFoundException ex) {
+		
+	}
+	
+	@ExceptionHandler(PasswordNotMatchException.class)
+	@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+	public void passwordNotMatchExceptionHandler(PasswordNotMatchException ex) {
 		
 	}
 	
