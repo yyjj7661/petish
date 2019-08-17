c267ef9fb89ca33b694e1cda9b2b4063044f0960
yyjj7661 <yyjj7661@gmail.com>
2019-08-18 00:38:58 +0900

    [MOD] 강아지 정모 마커의 위치를 재조정 했습니다.

--- src/main/webapp/resources/js/gatherboard/post.js
+++ src/main/webapp/resources/js/gatherboard/post.js
@@ -180,8 +180,7 @@ var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니
 var geocoder = new daum.maps.services.Geocoder();
 
 var imageSrc = '/resources/img/gatherboard/dog.png', // 마커이미지의 주소입니다    
-imageSize = new kakao.maps.Size(64, 60), // 마커이미지의 크기입니다
-imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+imageOption = {offset: new kakao.maps.Point(27, 45)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
   
 // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
 var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
